ba0da4d8c60560896c0fb2a76ac91ca0720891a7
saisunkari19 <saisunkari19@protonmail.com>
2023-12-08 12:34:36 +0530

    adding test file

--- src/utils/fn_add.rs
+++ src/utils/fn_add.rs
@@ -1,3 +1,18 @@
 pub fn add(num: u32) -> u32 {
     num + 5
 }
+
+
+#[cfg(test)]
+mod test{
+    use super::add;
+
+    #[test]
+    fn add_test(){
+        let x :i32 = 100;
+        let y = add(x.try_into().unwrap());
+        println!("x and y are {} {}", x, y);
+
+        assert_eq!(y, 105);
+    }
+}
