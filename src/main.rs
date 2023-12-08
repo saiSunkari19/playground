435e70b04675dd9d850881d621da8a5c4e77b60d
saisunkari19 <saisunkari19@protonmail.com>
2023-12-08 10:35:10 +0530

    adding function declarations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+fn add(num: u32) -> u32 {
+    num + 5
+}
+
+fn sum_of_array(arr: &[i32]) -> i32{
+    arr.iter().sum()
+}
+
+fn main(){
+    let x: i32 = 20;
+
+    let y = add(x.try_into().unwrap());
+
+    println!("x is {}", x);
+    println!("y is {}", y);
+
+    let arr = [1,2,3,4];
+
+    println!("Sum of array {}", sum_of_array(&arr));
 }
